309371ab90830e0cdd08f51ce506f7abc866a39d
Rohit Kumar Malik <rohit.malik@outlook.com>
2015-03-15 20:58:14 -0400

    Checking in the code changes
    Checking in the code changes

---
+++ Resto/app/src/main/java/com/example/rkmalik/resto/AppSettingActivity.java
@@ -0,0 +1,45 @@
+package com.example.rkmalik.resto;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class AppSettingActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_setting);
+        Intent intent = getIntent();
+        //String value = intent.getStringExtra("key");
+        String value = "Rohit Malik";
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_app_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Resto/app/src/main/java/com/example/rkmalik/resto/RestaurantListViewFragment.java
@@ -0,0 +1,63 @@
+package com.example.rkmalik.resto;
+
+import android.app.ListFragment;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantListViewFragment extends ListFragment {
+    private List<Rest_ListViewItem_Holder> mItems;        // ListView items list
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Integer[] imageId = {
+                R.drawable.subway,
+                R.drawable.chipotle,
+                R.drawable.polotropical,
+                R.drawable.wendys,
+                R.drawable.mcdonals,
+        };
+
+        String [] key_array =  getResources().getStringArray(R.array.restaurant_list);
+        
+        // initialize the items list
+        mItems = new ArrayList<Rest_ListViewItem_Holder>();
+        Resources resources = getResources();
+        
+        /*mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(R.drawable.subway), key_array[0], "Rohit Kumar Malik"));
+        mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(R.drawable.chipotle), key_array[1], "Rohit Kumar Malik"));
+        mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(R.drawable.polotropical), key_array[2], "Rohit Kumar Malik"));
+        mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(R.drawable.wendys), key_array[3], "Rohit Kumar Malik"));
+        mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(imageId[4]), key_array[4], "Rohit Kumar Malik"));*/
+
+        for (int i = 0; i < imageId.length; i++) {
+            mItems.add(new Rest_ListViewItem_Holder(resources.getDrawable(imageId[i]), key_array[i], "Rohit Kumar Malik"));
+        }
+
+        // initialize and set the list adapter
+        setListAdapter(new RestaurantListAdapter(getActivity(), mItems));
+    }
+    
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // remove the dividers from the ListView of the ListFragment
+        getListView().setDivider(null);
+    }
+ 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // retrieve theListView item
+        Rest_ListViewItem_Holder item = mItems.get(position);
+        
+        // do something
+        Toast.makeText(getActivity(), item.title, Toast.LENGTH_SHORT).show();
+    }
+}
